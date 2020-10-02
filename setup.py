91781672d5a7c0ba555643998158d714d5614fd4
Emre <mail@emreyilmaz.me>
2020-10-02 17:34:29 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='rabona_python',
+    url='https://github.com/emre/rabona_python',
+    version='0.0.1',
+    py_modules=['rabona_python'],
+    install_requires=[
+        'requests',
+    ],
+)
